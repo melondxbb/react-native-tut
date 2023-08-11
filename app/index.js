c5b003460ff8e4abfcbd38ae41114f4b2ad9c00f
Sana Ullah <Sana.Ullah@osn.com>
2023-08-11 17:40:21 +0400

    added fixes

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
 
 
 const Home = () => {
+    
     return (
         <View>
+            
             <Text>Home</Text>
         </View>
     );
